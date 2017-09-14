8294f7e3eab2d96fb56470192eaa24808829379f
Jerome Toole <jerometoole@gmail.com>
2017-09-14 16:35:40 +0100

    Audio Player working with play and pause (not styled)
    Audio select field added for homepage

---
+++ site/snippets/audioplayer.php
@@ -0,0 +1,26 @@
+<div class="audio-player-wrap audiojs">
+
+      <audio>
+        Your browser does not support the <code>audio</code> element.
+        <source src="<?php echo $file ?>" type="audio/mp3">
+      </audio>
+      <div class="audio-player">
+        <div class="play-pause">
+          <div class="play"></div>
+          <div class="pause"></div>
+          <div class="loading"></div>
+          <div class="error"></div>
+        </div>
+        <div class="player-controls">
+          <div class="scrubber">
+            <div class="progress"></div>
+            <div class="loaded"></div>
+          </div>
+        </div>
+
+        <div class="time"><small class="played">00:00</small>/<small class="duration">00:00</small></div>
+        <div class="error-message"></div>
+      </div>
+
+  
+</div>
--- site/templates/home.php
+++ site/templates/home.php
@@ -25,66 +25,14 @@
 
 
     <section class="reels wrap">
-      <?php if($file = $page->file('sample.mp3')): ?>
-      <div class="audio-player-wrap audiojs">
-    
-            <audio>
-              Your browser does not support the <code>audio</code> element.
-              <source src="<?php echo $file->url() ?>" type="audio/mp3">
-            </audio>
-            <div class="audio-player">
-              <div class="play-pause">
-                <p class="play">play</p>
-                <p class="pause"></p>
-                <p class="loading"></p>
-                <p class="error"></p>
-              </div>
-              <div class="player-controls">
-                <div class="scrubber">
-                  <div class="progress"></div>
-                  <div class="loaded"></div>
-                </div>
-              </div>
+      <?php if($page->commerciallink()): ?>
+        <?php $commerciallink = $page->commerciallink()->toFile()->url() ?>
+        <?php snippet('audioplayer', array('file' => $commerciallink)) ?>
+      <?php endif ?>
 
-              <div class="time"><em class="played">00:00</em>/<strong class="duration">00:00</strong></div>
-              <div class="error-message"></div>
-            </div>
-
-        
-      </div>
-      <?php endif ?>  
-
-      <?php if($file = $page->file('sample.mp3')): ?>
-      <div class="audio-player-wrap audiojs">
-      
-            <audio>
-              Your browser does not support the <code>audio</code> element.
-              <source src="<?php echo $file->url() ?>" type="audio/mp3">
-            </audio>
-            <div class="audio-player">
-              <div class="controls">
-              
-                <div class="play-pause">
-                  <p class="play">play</p>
-                  <p class="pause"></p>
-                  <p class="loading"></p>
-                  <p class="error"></p>
-                </div>
-              
-                <div class="player-controls">
-                  <div class="scrubber">
-                    <div class="progress"></div>
-                    <div class="loaded"></div>
-                  </div>
-                </div>
-
-                <div class="time"><em class="played">00:00</em>/<strong class="duration">00:00</strong></div>
-                <div class="error-message"></div>
-              </div>
-            </div>
-
-        
-      </div>
+      <?php if($page->commerciallink()): ?>
+        <?php $commerciallink = $page->commerciallink()->toFile()->url() ?>
+        <?php snippet('audioplayer', array('file' => $commerciallink)) ?>
       <?php endif ?>
     
     </section>
